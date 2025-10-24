dfcc18aeeda267ac278eab1648e0b471e47fff27
Mostafa Rasouli <Kasraco@gmail.com>
2025-10-24 18:21:20 +0330

    Refactored ExpertiseSection to use data from expertiseData file instead of props

--- components/ExpertiseSection.tsx
+++ components/ExpertiseSection.tsx
@@ -5,15 +5,13 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Skills } from "./Skills";
 import { fadeInLeft } from "@/lib/motion/variants";
 import dynamic from "next/dynamic";
+import { expertiseData } from "@/data/expertiseData";
 
 const MotionBlock = dynamic(() => import("./motions/Block"));
 
-interface ExpertiseProps {
-  title: string;
-  items?: string[];
-}
+export default function ExpertiseSection() {
+  const {title, expertisItems}=expertiseData;
 
-export default function ExpertiseSection({ title, items = ["HEALTHTECH", "E-COMMERCE", "IOT", "SAAS", "MANUFACTURE"] }: ExpertiseProps) {
   return (
     <MotionBlock variants={{
       ...fadeInLeft, animate: {
@@ -30,7 +28,7 @@ export default function ExpertiseSection({ title, items = ["HEALTHTECH", "E-COMM
           <div className="flex flex-col sm:flex-row gap-4">
             <div className="space-y-3 sm:w-4/12">
               <h3 className="text-nowrap">{title}</h3>
-              {items.map((item) => (
+              {expertisItems.map((item) => (
                 <div key={item} className="flex items-center space-x-2">
                   <Check className="w-5 h-5 text-green-500 dark:text-green-400" />
                   <span className="font-mono text-sm">{item}</span>
